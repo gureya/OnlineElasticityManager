ca5a793fe4583ef835b554ca672f8bf8ac220dec
Gureya Daharewa Davis <gureya.daharewa@gmail.com>
2015-08-18 23:03:32 +0200

    kill cassandra after decommisioning

--- src/actuator/removeInstances.sh
+++ src/actuator/removeInstances.sh
@@ -3,4 +3,7 @@ machineuser=ubuntu
 for i in "$@"
 do
 ssh  $machineuser@$i "/home/ubuntu/cassandra/bin/nodetool -h $i decommission"
+ssh $machineuser@$i "sudo rm -rf /var/lib/cassandra/*"
+pid=$(ssh $machineuser@$i "ps aux | grep '[c]assandra'" | awk '{print $2}')
+ssh $machineuser@$i "kill -9 $pid"
 done
